98424a895ad6f2a5d7556de29d3b45c685379a89
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-06-16 19:44:38 -0500

    [add] - Initial commit  🚀

---
+++ sidebarMenu/sidebarMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  sidebarMenuApp.swift
+//  sidebarMenu
+//
+//  Created by Pierre Juarez U. on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct sidebarMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
